0675ad497b0ab0342a35468c1ea273b63255a3d4
ZYFDroid <zyfdroid@qq.com>
2022-02-25 11:06:04 +0800

    内联组件减少卡顿

--- src/views/CoverView.vue
+++ src/views/CoverView.vue
@@ -3,25 +3,44 @@
     <v-container fluid>
       <v-row dense class="d-flex justify-center">
         <v-col v-for="book in books" :key="book.UUID">
-          <book-item
-            :uuid="book.UUID"
-            :name="book.displayName"
-          ></book-item> </v-col
-      ></v-row>
+         <v-card width="140px">
+           <v-img
+             :src="'/api/cover/'+book.UUID"
+             class="white--text align-end"
+             gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+             height="200px"
+             width="140px"
+             @click="readbook(book.UUID)"
+           >
+             <!-- <v-spacer></v-spacer>{{ name }}<v-spacer></v-spacer> -->
+             <v-card-title v-text="name" class="caption "></v-card-title
+           ></v-img>
+           <v-divider></v-divider>
+           <v-card-actions>
+             <v-spacer></v-spacer>
+             <v-btn text @click="readbook(book.UUID)">阅读</v-btn>
+           </v-card-actions>
+        </v-card>
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
-import BookItem from "../components/BookItem.vue";
 export default {
-  components: { BookItem },
+  components: {  },
   name: "CoverView",
   props: ["books"],
   mounted() {
     console.log("books");
   },
-  methods: {},
+  methods: {
+    readbook(uuid) {
+      this.$router.push("/read/" + uuid);
+      // alert(uuid)
+      // window.location.replace("/api/open/" + uuid)
+    }
+  },
 };
 </script>
 
